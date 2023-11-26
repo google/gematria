5277ceee32228ccd1566cbaf80cc364713285863
z277zhu <z277zhu@uwaterloo.ca>
2023-11-26 12:59:24 -0500

    Fixed code style & Setup llvm-exegesis register-def during runtime

--- gematria/datasets/convert_bhive_to_llvm_exegesis_input.cc
+++ gematria/datasets/convert_bhive_to_llvm_exegesis_input.cc
@@ -27,22 +27,21 @@
 #include "gematria/llvm/llvm_architecture_support.h"
 #include "gematria/utils/string.h"
 
-#define INITIAL_REG_VALUE 0x10000
-#define INITIAL_MEM_VALUE 2147483647
-#define LLVM_EXEGESIS_MEM_DEF_PREFIX "# LLVM-EXEGESIS-MEM-DEF "
-#define LLVM_EXEGESIS_MEM_MAP_PREFIX "# LLVM-EXEGESIS-MEM-MAP "
-#define LLVM_EXEGESIS_MEM_NAME_PREFIX "MEM"
+constexpr uint64_t kInitialRegVal = 10000;
+constexpr uint64_t kInitialMemVal = 2147483647;
+constexpr std::string_view kRegDefPrefix = "# LLVM-EXEGESIS-DEFREG ";
+constexpr std::string_view kMemDefPrefix = "# LLVM-EXEGESIS-MEM-DEF ";
+constexpr std::string_view kMemMapPrefix = "# LLVM-EXEGESIS-MEM-MAP ";
+constexpr std::string_view kMemNamePrefix = "MEM";
 
-static unsigned int file_counter = 0;
+namespace {
+unsigned int file_counter = 0;
+}
 
-ABSL_FLAG(std::string, bhive_csv, "",
-          "Filename of the input file containing code hex");
+ABSL_FLAG(std::string, bhive_csv, "", "Filename of the input BHive CSV file");
 ABSL_FLAG(
     std::string, output_dir, "",
     "Directory containing output files that can be executed by llvm-exegesis");
-ABSL_FLAG(std::string, exegesis_template,
-          "gematria/datasets/llvm-exegesis_warpper.S",
-          "Template file for llvm-exegesis");
 
 int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
@@ -59,20 +58,28 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  // open template llvm-exegesis file
-  std::string template_file_path = absl::GetFlag(FLAGS_exegesis_template);
-  std::string template_content;
-
-  // Read the content of the template file
-  {
-    std::ifstream template_file(template_file_path);
-    std::stringstream buffer;
-    buffer << template_file.rdbuf();
-    template_content = buffer.str();
-  }
-
+  std::string register_defs_lines;
   const std::unique_ptr<gematria::LlvmArchitectureSupport> llvm_support =
       gematria::LlvmArchitectureSupport::X86_64();
+  const llvm::MCRegisterInfo& MRI = llvm_support->mc_register_info();
+  for (llvm::MCPhysReg I = 1, E = MRI.getNumRegs(); I != E; ++I) {
+    // Filter out subregisters. Only include the super register.
+    bool is_sub_reg = false;
+    for (auto SuperReg : MRI.superregs(I)) {
+      if (MRI.isSubRegister(SuperReg, I)) {
+        is_sub_reg = true;
+        break;
+      }
+    }
+    if (is_sub_reg) {
+      continue;
+    }
+    // Append register definition line.
+    llvm::StringRef reg_name = MRI.getName(I);
+    register_defs_lines += std::string(kRegDefPrefix) + std::string(reg_name) +
+                           " " + std::to_string(kInitialRegVal) + "\n";
+  }
+
   gematria::X86Canonicalizer canonicalizer(&llvm_support->target_machine());
   gematria::BHiveImporter bhive_importer(&canonicalizer);
 
@@ -85,63 +92,62 @@ int main(int argc, char* argv[]) {
     }
 
     std::string_view hex = std::string_view(line).substr(0, comma_index);
-    auto bytes_or = gematria::ParseHexString(hex);
-    if (!bytes_or.has_value()) {
+    // For each line, find the accessed addresses & disassemble instructions.
+    auto bytes = gematria::ParseHexString(hex);
+    if (!bytes.has_value()) {
       std::cerr << "could not parse: " << hex << "\n";
       return 3;
     }
 
-    // for each line, find the accessed addresses & disassemble instructions
-    const auto& bytes = bytes_or.value();
+    // This will only get the first segfault address.
+    auto addrs = gematria::FindAccessedAddrs(*bytes);
+    auto proto = bhive_importer.BasicBlockProtoFromMachineCode(*bytes);
 
-    // this will only get the first segfault address
-    auto addrs_or = gematria::FindAccessedAddrs(bytes);
-    auto proto = bhive_importer.BasicBlockProtoFromMachineCode(bytes);
-    if (addrs_or.ok() && proto.ok()) {
-      // Create output file path
-      std::string output_file_path =
-          output_dir + "/" + std::to_string(file_counter) + ".test";
-
-      // Open output file for writing
-      std::ofstream output_file(output_file_path);
-      if (!output_file.is_open()) {
-        std::cerr << "Failed to open output file: " << output_file_path << "\n";
-        return 4;
-      }
-
-      // Write the template content into the output file
-      output_file << template_content;
-
-      // Append memory annotations
-      auto addrs = addrs_or.value();
-
-      // Multiple mappings can point to the same definition
-      if (addrs.accessed_blocks.size() > 0) {
-        output_file << LLVM_EXEGESIS_MEM_DEF_PREFIX
-                    << LLVM_EXEGESIS_MEM_NAME_PREFIX << " " << addrs.block_size
-                    << " " << INITIAL_MEM_VALUE << "\n";
-      }
-      for (const auto& addr : addrs.accessed_blocks) {
-        output_file << LLVM_EXEGESIS_MEM_MAP_PREFIX
-                    << LLVM_EXEGESIS_MEM_NAME_PREFIX << " " << std::dec << addr
-                    << "\n";
-      }
-
-      // Append disassembled instructions
-      for (const auto& instr : proto->machine_instructions()) {
-        output_file << instr.assembly() << "\n";
-      }
-
-      file_counter++;
-    } else if (proto.ok()) {
+    // Check for errors.
+    if (!proto.ok()) {
+      std::cerr << "Failed to disassemble block '" << hex << ": "
+                << proto.status() << "\n";
+      continue;
+    }
+    if (!addrs.ok()) {
       std::cerr << "Failed to find addresses for block '" << hex
-                << "': " << addrs_or.status() << "\n";
+                << "': " << addrs.status() << "\n";
       std::cerr << "Block disassembly:\n";
       for (const auto& instr : proto->machine_instructions()) {
         std::cerr << "\t" << instr.assembly() << "\n";
       }
-    } else {
-      std::cerr << "Failed to disassemble block '" << hex << "\n";
+      continue;
     }
+
+    // Create output file path.
+    std::string output_file_path =
+        output_dir + "/" + std::to_string(file_counter) + ".test";
+
+    // Open output file for writing.
+    std::ofstream output_file(output_file_path);
+    if (!output_file.is_open()) {
+      std::cerr << "Failed to open output file: " << output_file_path << "\n";
+      return 4;
+    }
+
+    // Write the register definition lines into the output file.
+    output_file << register_defs_lines;
+
+    // Multiple mappings can point to the same definition.
+    if (addrs->accessed_blocks.size() > 0) {
+      output_file << kMemDefPrefix << kMemNamePrefix << " " << addrs->block_size
+                  << " " << kInitialMemVal << "\n";
+    }
+    for (const auto& addr : addrs->accessed_blocks) {
+      output_file << kMemMapPrefix << kMemNamePrefix << " " << std::dec << addr
+                  << "\n";
+    }
+
+    // Append disassembled instructions.
+    for (const auto& instr : proto->machine_instructions()) {
+      output_file << instr.assembly() << "\n";
+    }
+
+    file_counter++;
   }
 }
