a1317edb049e8278730e10256e2b76b6c21415f4
z277zhu <z277zhu@uwaterloo.ca>
2023-12-04 14:24:30 -0500

    Fixed simplify LI script

--- scripts/bhive_simplify_LI.py
+++ scripts/bhive_simplify_LI.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 def simplify_live_intervals(input_file, output_file):
     with open(input_file, 'r') as file:
@@ -12,7 +13,6 @@ def simplify_live_intervals(input_file, output_file):
     previous_line_end = None
 
     for line in lines:
-
         # Extract function name using regex
         if machine_instrs_section and line.startswith('# Machine code for function'):
             match = re.search(r'function\s+([^\s:]+)', line)
@@ -49,12 +49,14 @@ def simplify_live_intervals(input_file, output_file):
         if '# End machine code for function' in line:
             machine_instrs_section = False
             intervals_section = False
+            if basic_block_name is not None:
+                simplified_output.append(f"{basic_block_name}: {basic_block_start}B {previous_line_end}B\n")
+                basic_block_name = None
             simplified_outputs.append(simplified_output)
             continue
 
         if intervals_section:
-            if line.startswith('%'):
-                simplified_output.append(line)
+            simplified_output.append(line)
         if machine_instrs_section:
             match = re.search(r'bb\.\d+\.(BB_\d+)', line)
             if match:
@@ -71,6 +73,11 @@ def simplify_live_intervals(input_file, output_file):
         for simplified_output in simplified_outputs:
             file.writelines(simplified_output)
 # Example usage
-input_file = 'log.txt'  # replace with your input file name
-output_file = 'output.txt'  # replace with your desired output file name
-simplify_live_intervals(input_file, output_file)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python bhive_simplify_LI.py <input_file> <output_file>")
+        sys.exit(1)
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    simplify_live_intervals(input_file, output_file)
