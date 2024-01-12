6f795a0df892d67e9cc42dab4e46598ae54aa6d1
z277zhu <z277zhu@uwaterloo.ca>
2024-01-11 20:43:23 -0500

    Fixed variable naming style & using llvm::Twine for string concat & tiny nitpicks

--- gematria/datasets/convert_bhive_to_llvm_exegesis_input.cc
+++ gematria/datasets/convert_bhive_to_llvm_exegesis_input.cc
@@ -29,8 +29,8 @@
 #include "gematria/llvm/llvm_architecture_support.h"
 #include "gematria/utils/string.h"
 
-constexpr uint64_t kInitialRegVal = 65536;
-constexpr uint64_t kInitialMemVal = 2147483647;
+constexpr uint64_t kInitialRegVal = 0x10000;
+constexpr uint64_t kInitialMemVal = 0x7FFFFFFF;
 constexpr std::string_view kRegDefPrefix = "# LLVM-EXEGESIS-DEFREG ";
 constexpr std::string_view kMemDefPrefix = "# LLVM-EXEGESIS-MEM-DEF ";
 constexpr std::string_view kMemMapPrefix = "# LLVM-EXEGESIS-MEM-MAP ";
@@ -60,24 +60,24 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  std::string initialRegValStr;
+  std::string initial_reg_val_str;
   {
-    // initialize initialRegValStr with kInitialRegValue in hex
+    // initialize initial_reg_val_str with kInitialRegValue in hex
     std::stringstream ss;
     ss << std::hex << kInitialRegVal << std::dec;
-    ss >> initialRegValStr;
+    ss >> initial_reg_val_str;
   }
-  std::string initialMemValStr;
+  std::string initial_mem_val_str;
   {
-    // initialize initialMemValStr with kInitialMemValue in hex
+    // initialize initial_mem_val_str with kInitialMemValue in hex
     std::stringstream ss;
     ss << std::hex << kInitialMemVal << std::dec;
-    ss >> initialMemValStr;
+    ss >> initial_mem_val_str;
   }
   std::string register_defs_lines;
   const std::unique_ptr<gematria::LlvmArchitectureSupport> llvm_support =
       gematria::LlvmArchitectureSupport::X86_64();
-  const llvm::MCRegisterInfo& MRI = llvm_support->mc_register_info();
+  const llvm::MCRegisterInfo& reg_info = llvm_support->mc_register_info();
 
   // Iterate through all general purpose registers and vector registers
   // and add them to the register definitions.
@@ -85,18 +85,29 @@ int main(int argc, char* argv[]) {
   // the LLVM version is bumped to avoid including the new APX GPRs (r16-r31)
   // that have recently been added to LLVM.
   for (unsigned i = 0;
-       i < MRI.getRegClass(llvm::X86::GR64_NOREXRegClassID).getNumRegs(); ++i) {
-    llvm::StringRef reg_name = MRI.getName(
-        MRI.getRegClass(llvm::X86::GR64_NOREXRegClassID).getRegister(i));
-    register_defs_lines += std::string(kRegDefPrefix) + std::string(reg_name) +
-                           " " + initialRegValStr + "\n";
+       i < reg_info.getRegClass(llvm::X86::GR64_NOREXRegClassID).getNumRegs();
+       ++i) {
+    llvm::StringRef reg_name = reg_info.getName(
+        reg_info.getRegClass(llvm::X86::GR64_NOREXRegClassID).getRegister(i));
+    register_defs_lines = llvm::Twine(register_defs_lines)
+                              .concat(kRegDefPrefix)
+                              .concat(reg_name)
+                              .concat(" ")
+                              .concat(initial_reg_val_str)
+                              .concat("\n")
+                              .str();
   }
   for (unsigned i = 0;
-       i < MRI.getRegClass(llvm::X86::VR128RegClassID).getNumRegs(); ++i) {
-    llvm::StringRef reg_name =
-        MRI.getName(MRI.getRegClass(llvm::X86::VR128RegClassID).getRegister(i));
-    register_defs_lines += std::string(kRegDefPrefix) + std::string(reg_name) +
-                           " " + initialRegValStr + "\n";
+       i < reg_info.getRegClass(llvm::X86::VR128RegClassID).getNumRegs(); ++i) {
+    llvm::StringRef reg_name = reg_info.getName(
+        reg_info.getRegClass(llvm::X86::VR128RegClassID).getRegister(i));
+    register_defs_lines = llvm::Twine(register_defs_lines)
+                              .concat(kRegDefPrefix)
+                              .concat(reg_name)
+                              .concat(" ")
+                              .concat(initial_reg_val_str)
+                              .concat("\n")
+                              .str();
   }
 
   gematria::X86Canonicalizer canonicalizer(&llvm_support->target_machine());
@@ -118,8 +129,6 @@ int main(int argc, char* argv[]) {
       return 3;
     }
 
-    // This will only get the first segfault address.
-    auto addrs = gematria::FindAccessedAddrs(*bytes);
     auto proto = bhive_importer.BasicBlockProtoFromMachineCode(*bytes);
 
     // Check for errors.
@@ -128,6 +137,10 @@ int main(int argc, char* argv[]) {
                 << proto.status() << "\n";
       continue;
     }
+
+    // This will only get the first segfault address.
+    auto addrs = gematria::FindAccessedAddrs(*bytes);
+
     if (!addrs.ok()) {
       std::cerr << "Failed to find addresses for block '" << hex
                 << "': " << addrs.status() << "\n";
@@ -139,13 +152,16 @@ int main(int argc, char* argv[]) {
     }
 
     // Create output file path.
-    std::string output_file_path =
-        output_dir + "/" + std::to_string(file_counter) + ".test";
+    llvm::Twine output_file_path = llvm::Twine(output_dir)
+                                       .concat("/")
+                                       .concat(llvm::Twine(file_counter))
+                                       .concat(".test");
 
     // Open output file for writing.
-    std::ofstream output_file(output_file_path);
+    std::ofstream output_file(output_file_path.str());
     if (!output_file.is_open()) {
-      std::cerr << "Failed to open output file: " << output_file_path << "\n";
+      std::cerr << "Failed to open output file: " << output_file_path.str()
+                << "\n";
       return 4;
     }
 
@@ -155,7 +171,7 @@ int main(int argc, char* argv[]) {
     // Multiple mappings can point to the same definition.
     if (addrs->accessed_blocks.size() > 0) {
       output_file << kMemDefPrefix << kMemNamePrefix << " " << addrs->block_size
-                  << " " << initialMemValStr << "\n";
+                  << " " << initial_mem_val_str << "\n";
     }
     for (const auto& addr : addrs->accessed_blocks) {
       output_file << kMemMapPrefix << kMemNamePrefix << " " << std::dec << addr
