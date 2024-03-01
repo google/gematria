0bed61a0d8d8f4d532a3b9766f136e7aa3cc81ff
Aiden Grossman <agrossman154@yahoo.com>
2024-02-29 18:51:59 -0800

    Minor adjustments

--- gematria/datasets/tests/lit.cfg.py
+++ gematria/datasets/tests/lit.cfg.py
@@ -3,7 +3,7 @@ import lit.formats
 config.name = 'gematria'
 config.test_format = lit.formats.ShTest(True)
 
-config.suffixes = ['.csv']
+config.suffixes = ['.test']
 
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.obj_root, 'test')
