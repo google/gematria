b249e8ca5497404b68fcd541b22bc092ff9383fe
Aiden Grossman <agrossman154@yahoo.com>
2024-02-29 18:42:01 -0800

    Get initial implementation working

--- gematria/datasets/tests/lit.cfg.py
+++ gematria/datasets/tests/lit.cfg.py
@@ -7,3 +7,10 @@ config.suffixes = ['.csv']
 
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.obj_root, 'test')
+
+config.substitutions.append(('FileCheck',
+        os.path.join(config.llvm_tools_root, 'FileCheck')))
+config.substitutions.append(("split-file",
+    os.path.join(config.llvm_tools_root, "split-file")))
+config.substitutions.append(('%convert_bhive_to_llvm_exegesis_input',
+    os.path.join(config.tools_root, 'convert_bhive_to_llvm_exegesis_input')))
--- gematria/datasets/tests/lit.site.cfg.py
+++ gematria/datasets/tests/lit.site.cfg.py
@@ -1,7 +1,8 @@
 import os
 
-config.src_root = r'./'
-config.obj_root = r'./'
+config.obj_root = os.path.join(os.getcwd(), 'gematria/datasets/tests')
+config.tools_root = os.path.join(os.getcwd(), 'gematria/datasets')
+config.llvm_tools_root = os.path.join(os.getcwd(), 'external/llvm-project/llvm')
 
 lit_config.load_config(
-        config, os.path.join(config.src_root, "lit.cfg.py"))
+        config, os.path.join(config.obj_root, 'lit.cfg.py'))
