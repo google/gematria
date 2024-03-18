682763793c882c744bce27a7f19f690251755dbd
Aiden Grossman <agrossman154@yahoo.com>
2024-03-18 12:41:11 -0700

    Make exegesis annotator error out when remapping an address (#73)
    Currently, there is a failure condition seen in some blocks where the
    annotator finds a segfault, tells exegesis to map a page there, and then
    the block fails again at the same address. This needs some debugging,
    but currently it will be helpful if we can just log a warning message
    and skip this basic block.

--- gematria/datasets/find_accessed_addrs_exegesis_test.cc
+++ gematria/datasets/find_accessed_addrs_exegesis_test.cc
@@ -121,5 +121,17 @@ TEST_F(FindAccessedAddrsExegesisTest, ExegesisZeroAddressError) {
   ASSERT_FALSE(static_cast<bool>(AddrsOrErr));
 }
 
+TEST_F(FindAccessedAddrsExegesisTest, ExegesisMultipleSameAddressError) {
+  // Try and load memory from an address above the current user space address
+  // space ceiling (assuming five level page tables are not enabled) as the
+  // script will currently try and annotate this, but exegesis will fail
+  // to map the address when it attempts to.
+  auto AddrsOrErr = FindAccessedAddrsExegesis(R"asm(
+    movabsq $0x0000800000000000, %rax
+    movq (%rax), %rax
+  )asm");
+  ASSERT_FALSE(static_cast<bool>(AddrsOrErr));
+}
+
 }  // namespace
 }  // namespace gematria
