d0a5825463f7f384e44ccb4564a37ac1ac6c3966
Aiden Grossman <agrossman154@yahoo.com>
2024-03-21 18:48:30 -0700

    Revert "Bump LLVM version"
    This reverts commit 659abdcfa38780ae03f9dc71d257616d3a5c1c5f.
    
    Accidentally pushed this to main instead of a branch on my fork...

--- gematria/datasets/find_accessed_addrs_exegesis.cc
+++ gematria/datasets/find_accessed_addrs_exegesis.cc
@@ -69,7 +69,7 @@ Expected<std::unique_ptr<ExegesisAnnotator>> ExegesisAnnotator::create(
 
   std::unique_ptr<const SnippetRepetitor> SnipRepetitor =
       SnippetRepetitor::Create(Benchmark::RepetitionModeE::Duplicate,
-                               ExegesisState, X86::R8);
+                               ExegesisState);
 
   return std::unique_ptr<ExegesisAnnotator>(new ExegesisAnnotator(
       ExegesisState, std::move(*RunnerOrErr), std::move(SnipRepetitor)));
@@ -124,8 +124,8 @@ Expected<AccessedAddrs> ExegesisAnnotator::findAccessedAddrs(
   }
 
   while (true) {
-    std::unique_ptr<const SnippetRepetitor> SR = SnippetRepetitor::Create(
-        Benchmark::RepetitionModeE::Duplicate, State, X86::R8);
+    std::unique_ptr<const SnippetRepetitor> SR =
+        SnippetRepetitor::Create(Benchmark::RepetitionModeE::Duplicate, State);
     Expected<BenchmarkRunner::RunnableConfiguration> RCOrErr =
         Runner->getRunnableConfiguration(BenchCode, 10000, 0, *SR);
 
