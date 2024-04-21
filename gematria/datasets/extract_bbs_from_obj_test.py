879cc8c600455d6edf7a79c3862a654ec6ed49e2
Aiden Grossman <aidengrossman@google.com>
2024-04-21 11:47:25 +0000

    Address feedback

--- gematria/datasets/extract_bbs_from_obj_test.py
+++ gematria/datasets/extract_bbs_from_obj_test.py
@@ -1,4 +1,17 @@
-from lit.main import main
+# Copyright 2024 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import lit.main
 import sys
 
 # Lit expects the test folder path to be specifided on the command-line, which
@@ -8,4 +21,4 @@ sys.argv.append("./gematria/datasets/extract_bbs_from_obj_tests")
 sys.argv.append("-vv")
 
 if __name__ == "__main__":
-  main()
+  lit.main.main()
