6910f35e70be954d8b5c2bfff81e8880fdac7f2e
Aiden Grossman <aidengrossman@google.com>
2024-04-21 11:52:58 +0000

    Fix spacing

--- gematria/datasets/extract_bbs_from_obj_tests/lit.cfg.py
+++ gematria/datasets/extract_bbs_from_obj_tests/lit.cfg.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import lit.formats
 
 config.name = 'extract_bbs_from_obj_tests'
