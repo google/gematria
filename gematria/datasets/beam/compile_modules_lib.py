b7f5aa5154b36f4e2467f8971c4621f4d839a79b
Aiden Grossman <aidengrossman@google.com>
2024-07-22 16:44:56 +0000

    Some more work

--- gematria/datasets/beam/compile_modules_lib.py
+++ gematria/datasets/beam/compile_modules_lib.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import apache_beam as beam
 from rules_python.python.runfiles import runfiles
 
@@ -9,9 +23,10 @@ import subprocess
 class OptimizeModules(beam.DoFn):
   def __init__(self, optimization_pass_lists: list[str]):
     self.optimization_pass_lists = optimization_pass_lists
+    self.opt_path = 'opt' # TODO: use runfiles
   
-  def optimize_module(input_module: bytes, optimization_pass_list: list[str]) -> bytes:
-    command_vector = ['opt', f'passes={optimization_pass_list}']
+  def optimize_module(self, input_module: bytes, optimization_pass_list: list[str]) -> bytes:
+    command_vector = [self.opt_path, f'passes={optimization_pass_list}']
     with subprocess.Popen(command_vector, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as opt_process:
       (output_bc, stderr) = opt_process.communicate(input=input_module)
       del stderr # We do not need any stderr output.
