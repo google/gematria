0c5a147a3d58bb94df5772c77b5d27f9350ef804
Aiden Grossman <agrossman154@yahoo.com>
2024-07-23 21:42:58 +0000

    Switch to __builtin_unreachable

--- gematria/granite/graph_builder_model_inference.cc
+++ gematria/granite/graph_builder_model_inference.cc
@@ -78,7 +78,6 @@ constexpr std::string_view kGraphNEdgeTensorName = "GnnModelBase.num_edges";
 constexpr std::string_view kGraphNNodeTensorName = "GnnModelBase.num_nodes";
 constexpr std::string_view kInstructionNodeMaskTensorName =
     "GraphBuilderModelBase.instruction_node_mask";
-constexpr std::string_view kUnknownTensorName = "UnknownName";
 
 // Look-up table between `InputTensor` and input tensor names. Used by
 // `GetInputTensorName` and `GetInputTensorFromName` to create an efficient
@@ -129,7 +128,7 @@ constexpr std::string_view GetInputTensorName(const InputTensor tensor) {
       return name;
     }
   }
-  return kUnknownTensorName;
+  __builtin_unreachable();
 }
 
 // Get the input tensor associated with a name. `name` must be one of the
