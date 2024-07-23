a5b341264ff4e16ad4ece9a6bc17fde61791c49c
Aiden Grossman <aidengrossman@google.com>
2024-07-23 16:01:52 -0700

    Disable pytype module-attr around glorot_normal (#153)
    Other installations of tensorflow/tf_keras can have complicated `__init__.py`s that pytype isn't able to comprehend correctly. Disable pytype around the one case of this so we can utilize gematria with alternative dependency installations.

--- gematria/granite/python/token_graph_builder_model.py
+++ gematria/granite/python/token_graph_builder_model.py
@@ -319,6 +319,11 @@ class TokenGraphBuilderModel(graph_builder_model_base.GraphBuilderModelBase):
   def _create_graph_network_modules(
       self,
   ) -> Sequence[gnn_model_base.GraphNetworkLayer]:
+    # Disable module-attr around uses of tf_keras.initializers.glorot_normal as
+    # some tf_keras installations setup what is available in tf_keras.initializers
+    # using logic in __init__.py that pytype is not able to understand
+    # correctly.
+    # pytype: disable=module-attr
     mlp_initializers = {
         'w': tf_keras.initializers.glorot_normal(),
         'b': tf_keras.initializers.glorot_normal(),
@@ -326,6 +331,7 @@ class TokenGraphBuilderModel(graph_builder_model_base.GraphBuilderModelBase):
     embedding_initializers = {
         'embeddings': tf_keras.initializers.glorot_normal(),
     }
+    # pytype: enable=module-attr
     return (
         gnn_model_base.GraphNetworkLayer(
             module=graph_nets.modules.GraphIndependent(
