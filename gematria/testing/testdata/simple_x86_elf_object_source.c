d02d822be21452cf565dc7f7c3b136112a721c80
Viraj Shah <vbshah@google.com>
2024-08-28 16:29:26 +0200

    `clang-format` testdata source file.

--- gematria/testing/testdata/simple_x86_elf_object_source.c
+++ gematria/testing/testdata/simple_x86_elf_object_source.c
@@ -1,14 +1,13 @@
 int main() {
   // TODO(virajbshah): Make this do something meaningful.
   asm volatile(
-    "        movl    $1000000000, %ecx\n"
-    "        xorl    %eax, %eax\n"
-    "loop:\n"
-    "        movl    %ecx, %edx\n"
-    "        imull   %edx, %edx\n"
-    "        addl    %edx, %eax\n"
-    "        decl    %ecx\n"
-    "        jnz     loop"
-  );
+      "        movl    $1000000000, %ecx\n"
+      "        xorl    %eax, %eax\n"
+      "loop:\n"
+      "        movl    %ecx, %edx\n"
+      "        imull   %edx, %edx\n"
+      "        addl    %edx, %eax\n"
+      "        decl    %ecx\n"
+      "        jnz     loop");
   return 0;
 }
