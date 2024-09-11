8387a3bb90e28ef2fd22ec7893f372574f543858
Viraj Shah <shahvirajbiren@gmail.com>
2024-09-12 02:04:03 +0530

    Change the source of `rules_python.python.runfiles` to PyPI.
     * `runfiles` is provided both as part of the Bazel `rules_python`
       repository as `@rules_python//python:runfiles` and on PyPI as
       the `bazel-runfiles` pip package.
     * It could be imported from the Bazel repository as
       `rules_python.python.runfiles` when using WORKSPACE, but needed
       to be imported as `python.runfiles` when using Bzlmod.
     * However, the pip package is independent of whether WORKSPACE or
       Bzlmod is being used.
     * See https://github.com/bazelbuild/rules_python/issues/1679 for
       details.

--- gematria/datasets/python/extract_bbs_from_obj_test.py
+++ gematria/datasets/python/extract_bbs_from_obj_test.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from absl.testing import absltest
-from rules_python.python.runfiles import runfiles
+from runfiles import runfiles
 import os
 from gematria.datasets.python import extract_bbs_from_obj
 
