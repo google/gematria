0e4c1e83d83b94089c71ff3e26f51a8d427f73d2
Aiden Grossman <agrossman154@yahoo.com>
2024-12-31 02:07:15 +0000

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.4

--- gematria/datasets/pipelines/benchmark_cpu_scheduler.py
+++ gematria/datasets/pipelines/benchmark_cpu_scheduler.py
@@ -14,9 +14,6 @@
 
 import abc
 from typing_extensions import override
-from collections.abc import Iterable
-import os
-import re
 
 
 class BenchmarkScheduler(metaclass=abc.ABCMeta):
@@ -69,75 +66,3 @@ class NoSchedulingBenchmarkScheduler(BenchmarkScheduler):
   @override
   def verify(self):
     pass
-
-
-class DefaultBenchmarkScheduler(BenchmarkScheduler):
-  """A BenchmarkScheduler that schedules processes separately.
-
-  DefaultBenchmarkScheduler schedules the main process and the benchmark
-  subprocess on separate cores, making sure to reserve the second hyperthread
-  on the benchmarking core to prevent interference. It expects that the main
-  process is initially given a CPU Mask with three active threads, additionally
-  assuming that two of the threads are neighboring (part of the same core).
-  Errors are raised if these conditions are not met. The benchmarking core
-  returned is one of the two neighboring threads. The main process has its
-  COU mask limited to the thread that neighbors neither of the other threads.
-  """
-
-  def __init__(self):
-    self._cpu_mask = []
-
-  @staticmethod
-  def _get_neighboring_threads(cpu_index: int) -> list[int]:
-    with open(
-        f'/sys/devices/system/cpu/cpu{cpu_index}/topology/thread_siblings_list'
-    ) as thread_sibling_list_handle:
-      neighboring_threads_strings = re.split(
-          r'[-,]+', thread_sibling_list_handle.read().strip()
-      )
-      neighboring_threads = [
-          int(cpu_index_str) for cpu_index_str in neighboring_threads_strings
-      ]
-    return neighboring_threads
-
-  def _get_aux_core_and_hyperthread_pair(
-      self,
-      cpu_mask: Iterable[int],
-  ) -> tuple[int, list[int]]:
-    for cpu_index in cpu_mask:
-      neighboring_threads = self._get_neighboring_threads(cpu_index)
-      if len(neighboring_threads) != 2:
-        raise ValueError('Expected two hyperthreads per CPU.')
-
-      if (
-          neighboring_threads[0] in cpu_mask
-          and neighboring_threads[1] in cpu_mask
-      ):
-        cpus = list(cpu_mask)
-        cpus.remove(neighboring_threads[0])
-        cpus.remove(neighboring_threads[1])
-        return (cpus[0], [neighboring_threads[0], neighboring_threads[1]])
-    raise ValueError(
-        'Expected a pair of neighboring hyperthreads in the CPU mask.'
-    )
-
-  @override
-  def setup_and_get_benchmark_core(self) -> int | None:
-    cpu_mask = os.sched_getaffinity(0)
-
-    if len(cpu_mask) != 3:
-      raise ValueError('Expected to have three CPUs.')
-
-    aux_core, hyperthread_pair = self._get_aux_core_and_hyperthread_pair(
-        cpu_mask
-    )
-    os.sched_setaffinity(0, [aux_core])
-    self._cpu_mask = [aux_core]
-
-    return hyperthread_pair[0]
-
-  @override
-  def verify(self):
-    cpu_mask = list(os.sched_getaffinity(0))
-    if self._cpu_mask != cpu_mask:
-      raise ValueError('Expected the CPU mask to not change.')
