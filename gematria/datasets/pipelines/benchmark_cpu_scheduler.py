992299a97d594272310e5f60aee87ddefb250896
Aiden Grossman <aidengrossman@google.com>
2025-01-17 08:56:30 -0800

    Add default benchmark scheduler
    This patch adds a default benchmark scheduler. This scheduler assumes
    the worker performing the benchmarking has three hyperthreads available
    inside its CPU mask, with two of them being part of a pair (from the
    same core). It then selects one core from the pair to be used for the
    benchmarking subprocess, and selects the core outside of the pair to be
    used to run the main exegesis/python program. Verification is also added
    to ensure that nothing external to the program changes the process mask,
    or at least to catch it early if it does happen.
    
    Reviewers: orodley, ondrasej
    
    Reviewed By: ondrasej
    
    Pull Request: https://github.com/google/gematria/pull/271

--- gematria/datasets/pipelines/benchmark_cpu_scheduler.py
+++ gematria/datasets/pipelines/benchmark_cpu_scheduler.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 
 import abc
+from collections.abc import Collection
+import os
+import re
 from typing_extensions import override
 
 
@@ -66,3 +69,82 @@ class NoSchedulingBenchmarkScheduler(BenchmarkScheduler):
   @override
   def verify(self):
     pass
+
+
+def _get_neighboring_threads(cpu_index: int) -> list[int]:
+  with open(
+      f'/sys/devices/system/cpu/cpu{cpu_index}/topology/thread_siblings_list'
+  ) as thread_sibling_list_handle:
+    neighboring_threads_strings = re.split(
+        '[-,]+', thread_sibling_list_handle.read().strip()
+    )
+    neighboring_threads = [
+        int(cpu_index_str) for cpu_index_str in neighboring_threads_strings
+    ]
+  return neighboring_threads
+
+
+class DefaultBenchmarkScheduler(BenchmarkScheduler):
+  """A BenchmarkScheduler that schedules processes separately.
+
+  DefaultBenchmarkScheduler schedules the main process and the benchmark
+  subprocess on separate cores, making sure to reserve the second hyperthread
+  on the benchmarking core to prevent interference. It expects that the main
+  process is initially given a CPU Mask with three active threads, additionally
+  assuming that two of the threads are neighboring (part of the same core).
+  Errors are raised if these conditions are not met. The benchmarking core
+  returned is one of the two neighboring threads. The main process has its
+  CPU mask limited to the thread that neighbors neither of the other threads.
+  """
+
+  def __init__(self):
+    self._cpu_mask = []
+
+  def _get_aux_core_and_hyperthread_pair(
+      self,
+      cpu_mask: Collection[int],
+  ) -> tuple[int, list[int]]:
+    for cpu_index in cpu_mask:
+      neighboring_threads = _get_neighboring_threads(cpu_index)
+      if len(neighboring_threads) != 2:
+        raise ValueError('Expected two hyperthreads per CPU.')
+
+      if (
+          neighboring_threads[0] not in cpu_mask
+          or neighboring_threads[1] not in cpu_mask
+      ):
+        continue
+
+      cpus = list(cpu_mask)
+      cpus.remove(neighboring_threads[0])
+      cpus.remove(neighboring_threads[1])
+
+      if not cpus:
+        continue
+
+      return (cpus[0], [neighboring_threads[0], neighboring_threads[1]])
+    raise ValueError(
+        'Expected a pair of neighboring hyperthreads and an aux thread in the'
+        ' CPU mask.'
+    )
+
+  @override
+  def setup_and_get_benchmark_core(self) -> int | None:
+    cpu_mask = os.sched_getaffinity(0)
+
+    if len(cpu_mask) != 3:
+      raise ValueError('Expected to have three CPUs.')
+
+    aux_core, hyperthread_pair = self._get_aux_core_and_hyperthread_pair(
+        cpu_mask
+    )
+    os.sched_setaffinity(0, [aux_core])
+    self._cpu_mask = [aux_core]
+
+    return hyperthread_pair[0]
+
+  @override
+  def verify(self):
+    cpu_mask = list(os.sched_getaffinity(0))
+    if self._cpu_mask != cpu_mask:
+      raise ValueError('Expected the CPU mask to not change.')
