43dcda11d6d3521d414a71774122a49f9c071a52
Aiden Grossman <aidengrossman@google.com>
2025-04-20 12:15:46 +0200

    Port model_blocks to TF2
    This patch ports model_blocks and model_blocks_test to TF2. The major
    change is the removal of the add_residual_connection_function. This is
    not needed anymore given that everything will be constructed explicitly
    eagerly inside the forward pass of models building off of the residual
    connection layer.
    
    Reviewers: ondrasej
    
    Reviewed By: ondrasej
    
    Pull Request: https://github.com/google/gematria/pull/281

--- gematria/model/python/model_blocks_test.py
+++ gematria/model/python/model_blocks_test.py
@@ -13,174 +13,64 @@
 # limitations under the License.
 
 from gematria.model.python import model_blocks
-import numpy as np
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 class ResidualConnectionLayerTest(tf.test.TestCase):
 
   def test_same_shapes(self):
-    shape = (2, 4)
+    shape = tf.TensorShape((2, 4))
     dtype = tf.dtypes.float32
-    input_tensor = tf.placeholder(shape=shape, dtype=dtype)
-    residual_tensor = tf.placeholder(shape=shape, dtype=dtype)
-    residual_layer = model_blocks.ResidualConnectionLayer(name='residual')
-    output_tensor = residual_layer((input_tensor, residual_tensor))
-    self.assertEqual(output_tensor.shape, shape)
+    residual_layer = model_blocks.ResidualConnectionLayer(
+        layer_input_shapes=(shape, shape), name='residual'
+    )
 
     self.assertEmpty(residual_layer.weights)
 
-    with self.session() as sess:
-      input_array = np.array(
-          [[1, 2, 3, 4], [5, 6, 7, 8]], dtype=dtype.as_numpy_dtype
-      )
-      residual_array = np.array(
-          [[-1, 1, -1, 1], [-2, 2, -2, 2]], dtype=dtype.as_numpy_dtype
-      )
-      output_array = sess.run(
-          output_tensor,
-          feed_dict={
-              input_tensor: input_array,
-              residual_tensor: residual_array,
-          },
-      )
-      self.assertAllEqual(output_array, [[0, 3, 2, 5], [3, 8, 5, 10]])
+    input_array = tf.constant(
+        [[1, 2, 3, 4], [5, 6, 7, 8]], dtype=dtype.as_numpy_dtype
+    )
+    residual_array = tf.constant(
+        [[-1, 1, -1, 1], [-2, 2, -2, 2]], dtype=dtype.as_numpy_dtype
+    )
+    output = residual_layer((input_array, residual_array))
+    self.assertEqual(output.shape, shape)
+    self.assertAllEqual(output, [[0, 3, 2, 5], [3, 8, 5, 10]])
 
   def test_different_shapes(self):
-    input_shape = (None, 2)
-    residual_shape = (None, 3)
+    input_shape = tf.TensorShape((None, 2))
+    residual_shape = tf.TensorShape((None, 3))
     dtype = tf.dtypes.float32
-    input_tensor = tf.placeholder(shape=input_shape, dtype=dtype)
-    residual_tensor = tf.placeholder(shape=residual_shape, dtype=dtype)
-    residual_layer = model_blocks.ResidualConnectionLayer(name='residual')
-    output_tensor = residual_layer((input_tensor, residual_tensor))
-    output_tensor.shape.assert_is_compatible_with(input_shape)
+    residual_layer = model_blocks.ResidualConnectionLayer(
+        layer_input_shapes=(input_shape, residual_shape), name='residual'
+    )
 
     residual_layer_weights = residual_layer.weights
     self.assertLen(residual_layer_weights, 1)
     self.assertTrue(residual_layer_weights[0].shape.is_compatible_with((3, 2)))
 
-    with self.session() as sess:
-      sess.run(tf.global_variables_initializer())
-      # In the first test, we use all zeros for the residual array. Any linear
-      # projection must map this to zeros, thus the output must be the same as
-      # input_array.
-      input_array = np.array([[1, 2], [3, 4]], dtype=dtype.as_numpy_dtype)
-      residual_array = np.zeros((2, 3), dtype=dtype.as_numpy_dtype)
-      output_array = sess.run(
-          output_tensor,
-          feed_dict={
-              input_tensor: input_array,
-              residual_tensor: residual_array,
-          },
-      )
-      self.assertAllEqual(output_array, input_array)
+    # In the first test, we use all zeros for the residual array. Any linear
+    # projection must map this to zeros, thus the output must be the same as
+    # input_tensor.
+    input_tensor = tf.constant([[1, 2], [3, 4]], dtype=dtype)
+    residual_tensor = tf.zeros((2, 3), dtype=dtype)
+    output_tensor = residual_layer((input_tensor, residual_tensor))
+    self.assertAllEqual(output_tensor, input_tensor)
 
-      # In the second test, we use a non-zero array. We can't test for exact
-      # values because of the random initialization of the linear transformation
-      # but with probability one, the output is different from the input.
-      residual_array = np.ones((2, 3), dtype=dtype.as_numpy_dtype)
-      output_array = sess.run(
-          output_tensor,
-          feed_dict={
-              input_tensor: input_array,
-              residual_tensor: residual_array,
-          },
-      )
-      self.assertNotAllClose(output_array, input_array)
+    # In the second test, we use a non-zero array. We can't test for exact
+    # values because of the random initialization of the linear transformation
+    # but with probability one, the output is different from the input.
+    residual_tensor = tf.ones((2, 3), dtype=dtype.as_numpy_dtype)
+    output_tensor = residual_layer((input_tensor, residual_tensor))
+    self.assertNotAllClose(output_tensor, input_tensor)
 
-
-class AddResidualConnectioNTest(tf.test.TestCase):
-
-  def test_same_shapes(self):
-    shape = (2, 4)
-    dtype = tf.dtypes.float32
-    input_tensor = tf.placeholder(shape=shape, dtype=dtype)
-    residual_tensor = tf.placeholder(shape=shape, dtype=dtype)
-    output_tensor = model_blocks.add_residual_connection(
-        input_tensor, residual_tensor, name='residual'
-    )
-    self.assertEqual(output_tensor.shape, shape)
-
-    with self.session() as sess:
-      input_array = np.array(
-          [[1, 2, 3, 4], [5, 6, 7, 8]], dtype=dtype.as_numpy_dtype
-      )
-      residual_array = np.array(
-          [[-1, 1, -1, 1], [-2, 2, -2, 2]], dtype=dtype.as_numpy_dtype
-      )
-      output_array = sess.run(
-          output_tensor,
-          feed_dict={
-              input_tensor: input_array,
-              residual_tensor: residual_array,
-          },
-      )
-      self.assertAllEqual(output_array, [[0, 3, 2, 5], [3, 8, 5, 10]])
-
-  def test_different_shapes(self):
-    input_shape = (None, 2)
-    residual_shape = (None, 3)
-    dtype = tf.dtypes.float32
-    input_tensor = tf.placeholder(shape=input_shape, dtype=dtype)
-    residual_tensor = tf.placeholder(shape=residual_shape, dtype=dtype)
-    output_tensor = model_blocks.add_residual_connection(
-        input_tensor, residual_tensor, name='residual'
-    )
-    output_tensor.shape.assert_is_compatible_with(input_shape)
-
-    with self.session() as sess:
-      sess.run(tf.global_variables_initializer())
-      # In the first test, we use all zeros for the residual array. Any linear
-      # projection must map this to zeros, thus the output must be the same as
-      # input_array.
-      input_array = np.array([[1, 2], [3, 4]], dtype=dtype.as_numpy_dtype)
-      residual_array = np.zeros((2, 3), dtype=dtype.as_numpy_dtype)
-      output_array = sess.run(
-          output_tensor,
-          feed_dict={
-              input_tensor: input_array,
-              residual_tensor: residual_array,
-          },
-      )
-      self.assertAllEqual(output_array, input_array)
-
-      # In the second test, we use a non-zero array. We can't test for exact
-      # values because of the random initialization of the linear transformation
-      # but with probability one, the output is different from the input.
-      residual_array = np.ones((2, 3), dtype=dtype.as_numpy_dtype)
-      output_array = sess.run(
-          output_tensor,
-          feed_dict={
-              input_tensor: input_array,
-              residual_tensor: residual_array,
-          },
-      )
-      self.assertNotAllClose(output_array, input_array)
-
-
-class CastTest(tf.test.TestCase):
-
-  def test_int32_to_float_cast(self):
-    input_shape = (4, 24)
-    input_dtype = tf.dtypes.int32
-    output_dtype = tf.dtypes.float32
-    input_tensor = tf.placeholder(shape=input_shape, dtype=input_dtype)
-    cast = model_blocks.cast(output_dtype)
-    output_tensor = cast(input_tensor)
-
-    self.assertEqual(output_tensor.shape, input_shape)
-    self.assertEqual(output_tensor.dtype, output_dtype)
-
-    with self.session() as sess:
-      input_array = np.ones(input_shape, input_dtype.as_numpy_dtype)
-      output_array = sess.run(
-          output_tensor, feed_dict={input_tensor: input_array}
-      )
-      self.assertEqual(output_array.shape, input_shape)
-      self.assertEqual(output_array.dtype, output_dtype.as_numpy_dtype)
+  def test_cast_layer(self):
+    input_tensor = tf.constant([1, 2, 3, 4], dtype=tf.dtypes.float32)
+    cast_layer = model_blocks.CastLayer(dtype=tf.dtypes.float64)
+    output_tensor = cast_layer(input_tensor)
+    self.assertEqual(output_tensor.dtype, tf.dtypes.float64)
+    self.assertAllClose(input_tensor.numpy(), [1, 2, 3, 4])
 
 
 if __name__ == '__main__':
-  tf.disable_v2_behavior()
   tf.test.main()
