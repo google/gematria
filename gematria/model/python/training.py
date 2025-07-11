8ae12beef0712e1628a800f4831a8e22db3604ba
Aiden Grossman <agrossman154@yahoo.com>
2025-07-11 16:08:48 +0000

    formatting

--- gematria/model/python/training.py
+++ gematria/model/python/training.py
@@ -366,7 +366,10 @@ def _as_list(values: Sequence[float]) -> list[float]:
 
 
 def _unwrap_if_tensor(
-    maybe_tensor: tf.Tensor | Sequence[Sequence[float]] | Sequence[float] | float,
+    maybe_tensor: tf.Tensor
+    | Sequence[Sequence[float]]
+    | Sequence[float]
+    | float,
 ) -> Sequence[Sequence[float]] | Sequence[float] | float:
   """Unwraps `maybe_tensor` if it is a Tensor."""
   if tf.is_tensor(maybe_tensor):
